0590565715522c45cd7683ef1d7506af0be5e6eb
pinatflo <florian.pinat@gmail.com>
2013-12-10 13:19:38 +0100

    test

--- TopUV/src/main/java/com/if26/topuv/activities/MainActivity.java
+++ TopUV/src/main/java/com/if26/topuv/activities/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import com.if26.topuv.R;
 import com.if26.topuv.fragment.LoginFragment;
 
+//trololo
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
