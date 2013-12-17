fd0533e626bc6702f96345fe1b6a86a21dd7ba66
JulesHirault <jules.hirault@gmail.com>
2013-12-17 14:43:12 +0100

    ajout topUV.sql début de la création listeUV

---
+++ TopUV/src/main/java/com/if26/topuv/models/ListUV.java
@@ -0,0 +1,17 @@
+package com.if26.topuv.models;
+
+import com.if26.topuv.R;
+
+/**
+ * Created by Jules on 17/12/13.
+ */
+public class ListUV {
+
+public R.string[] listUV;
+
+    public void createList()
+    {
+
+    }
+
+}
