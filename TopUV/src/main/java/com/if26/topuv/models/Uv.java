8ede79d65e8079241eb15e589c17a82730fd9d04
JulesHirault <jules.hirault@gmail.com>
2013-12-27 18:51:20 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.idea/workspace.xml
    	local.properties

--- TopUV/src/main/java/com/if26/topuv/models/Uv.java
+++ TopUV/src/main/java/com/if26/topuv/models/Uv.java
@@ -1,16 +1,18 @@
 package com.if26.topuv.models;
 
-import com.if26.topuv.R;
-
 /**
  * Created by Jules on 17/12/13.
  */
-public class UV {
-
+public class Uv {
     public String id;
     public String label;
-    public int avg_mark;
+    public String id_description;
+    public String avg_mark;
+    public String id_category;
 
+    public Category category;
 
-
+    public String getListLabel(){
+        return "  " + this.id + " - " + this.label;
+    }
 }
