92611bf42f6fe73c02ea44c3fc2812a68ef205b6
JulesHirault <jules.hirault@gmail.com>
2013-12-28 17:21:53 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.idea/workspace.xml
    	local.properties

---
+++ htdocs/comment.php
@@ -0,0 +1,31 @@
+<?php
+require_once('database/database.php');
+require_once('model/student.php');
+require_once('model/comment.php');
+
+$parameters = array
+(
+        ':token' => null,
+        ':id_uv' => null
+);
+foreach($_POST as $key => $value)
+{
+        $parameters[":$key"] = $value;
+}
+
+$json = array(
+        'error' => true
+);
+
+$configDB = require_once('configDB.php');
+$db = new Database($configDB['dsn'], $configDB['username'], $configDB['password'], $configDB['options']);
+
+$comments = $db->selectSeveral('Comment', 'comment', 'comment.id_uv = :id_uv', array(':id_uv' => $parameters[':id_uv']));
+
+$json = array(
+    'error' => false,
+    'comment' => $comments
+);
+
+echo json_encode($json);
+?>
